81aa2ecb1e7e9e1d01cdba90f8c10c7ed619e2de
ketandhumal <kdhumal914@gmail.com>
2026-01-22 12:40:04 +0530

    Initial App setup

---
+++ client/src/axios.js
@@ -0,0 +1,14 @@
+// src/axios.jsvbvvbnvnvnnvnvb
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API || 'http://localhost:5000'//'/api', // Proxy from frontend NGINX to backend K8s service
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers['Authorization'] = `Bearer ${token}`;
+  return config;
+});
+
+export default instance;
