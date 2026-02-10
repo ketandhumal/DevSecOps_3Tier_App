9627f41bb93663248e8a047c8ce09ffaab8a8158
ketandhumal <kdhumal914@gmail.com>
2026-02-10 11:53:35 +0530

    axios baseurl setup

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  "http://localhost:5000" ||'/api', // Proxy from frontend NGINX to backend K8s service
+  baseURL: process.env.REACT_APP_API_URL || "/api",     // Proxy from frontend NGINX to backend K8s service
 });
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers["Authorization"] = `Bearer ${token}`;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
 export default instance;
+
 
 
