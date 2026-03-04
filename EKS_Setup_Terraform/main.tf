c2aff48d842d9d4b40aa0d42b0a29feeecc01280
Ketan Vijay Dhumal <95542980+ketandhumal@users.noreply.github.com>
2026-03-04 14:02:56 +0530

    Update main.tf

--- EKS_Setup_Terraform/main.tf
+++ EKS_Setup_Terraform/main.tf
@@ -142,9 +142,9 @@ resource "aws_eks_node_group" "ketan_devops" {
   instance_types = ["t2.medium"]
 
   scaling_config {
-    desired_size = 3
-    min_size     = 3
-    max_size     = 3
+    desired_size = 2
+    min_size     = 1
+    max_size     = 2
   }
 }
 
